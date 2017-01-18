466165b23d5c98bd34267983982d037d5e534413
Yukio Usuzumi <anohigisavay@gmail.com>
2017-01-19 00:11:34 +0800

    Dependent-typed list

--- src/NaiveStructures/List/DT.hs
+++ src/NaiveStructures/List/DT.hs
@@ -2,8 +2,9 @@ module NaiveStructures.List.DT where
 
 import GHC.TypeLits
 
-data List a = Nil | Cons a (List a)
-
 data DTList a (nat :: Nat) where
   DTNil :: DTList a 0
   DTCons :: a -> DTList a nat -> DTList a (nat + 1)
+
+head :: (1 <= l) => DTList a l -> a
+head (DTCons a _) = a
